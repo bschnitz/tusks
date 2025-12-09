c251b6f760e835ee808a78e8d967134f4dec5cd3
Benjamin Schnitzler <benjamin.schnitzler.github@e.mail.de>
2025-12-09 09:37:22 +0100

    feat: multiplicity of arguments

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 pub use tusks_macro::tusks;
-pub use tusks_lib::models::{Argument, LinkNode, Tusk, TusksNode};
+pub use tusks_lib::models::{Argument, LinkNode, Tusk, TusksNode, ArgumentMultiplicity};
+pub use tusks_lib::types::{Repeat, RepeatMin, RepeatMax, RepeatMinMax};
 pub use indexmap;
+pub use tusks_lib::clap;
