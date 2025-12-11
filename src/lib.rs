aaadb939e69585a31ea5c5c642793f1507bb077e
Benjamin Schnitzler <benjamin.schnitzler.github@e.mail.de>
2025-12-11 10:17:07 +0100

    feat: new approach which is now closely bound to clap

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
 pub use tusks_macro::tusks;
-pub use tusks_lib::models::{Argument, LinkNode, Tusk, TusksNode, ArgumentMultiplicity};
-pub use tusks_lib::types::{Repeat, RepeatMin, RepeatMax, RepeatMinMax};
 pub use indexmap;
 pub use tusks_lib::clap;
